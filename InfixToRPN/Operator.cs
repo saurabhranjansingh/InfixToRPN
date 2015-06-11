9bfb15d5cd97fe6efbdeeddef64b2c207dd7bd73
saurabhranjansingh <saurabhranjansingh@gmail.com>
2015-06-11 17:28:59 +0530

    Added code into repo

---
+++ InfixToRPN/Operator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfixToRPN
+{
+    class Operator
+    {
+        public string operatorCode { get; set; }
+        public int associativity { get; set; }
+        public int precedence { get; set; }
+    }
+}
